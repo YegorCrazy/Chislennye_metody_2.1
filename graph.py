6be494dc4fddf47794b074ababd3b49c27c41a56
YegorCrazy <egor2002288@gmail.com>
2021-11-06 14:37:04 +0300

    graphic improved

--- graph.py
+++ graph.py
@@ -9,8 +9,10 @@ y1 = [float(line) for line in file_y1]
 file_y2 = open("y2output", "r")
 y2 = [float(line) for line in file_y2]
 
-plt.scatter(x, y1, label='2 accuracy', color='b')
-plt.scatter(x, y2, label='4 accuracy', color='g')
+s = 2;
+
+plt.scatter(x, y1, label = '2 accuracy', color = 'b', s = s)
+plt.scatter(x, y2, label = '4 accuracy', color = 'g', s = s)
 plt.xlabel('x')
 plt.ylabel('y')
 plt.show()
